5a015981407d0f85d106f40d855c46b17812058d
muGGlez <p.suksawatt@gmail.com>
2024-01-12 17:03:31 +0700

    update result

--- bmi/index.php
+++ bmi/index.php
@@ -1,17 +1,16 @@
 <?php
     include "process.php";
 
-    if(isset($_POST["submit"])){
+    if(isset($_POST["submit"])) {
         $input_gender = $_POST["gender"];
         $input_weight = floatval($_POST["weight"]);
         $input_height = floatval($_POST["height"]);
     
         $result = "Your BMI is: " . calculateBMI($input_gender, $input_weight, $input_height);
-    
+        $result = BMIStatus($gender, $bmi);
+         
         $_POST["result"] = $result;
         
-        //$_GET["result"] = $result;        
-    
         // if ($gender == "male") {
         //     showBMIStatusMale($bmi);
         // } elseif ($gender == "female") {
@@ -19,33 +18,36 @@
         // }
     }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BMI</title>
+
+    <link rel="stylesheet" href="/thaiware_internship/bmi/css/style.css" />
+
 </head>
 
-<link rel="stylesheet" href="/thaiware_internship/bmi/css/style.css" />
-
 <body>
-<form method="post" action="">
-    <label for="gender">Gender:</label>
-    <select name="gender" required>
-        <option value="male">Male</option>
-        <option value="female">Female</option>
-    </select><br>
+    <form method="post" action="">
+        <label for="gender">Gender:</label>
+        <select name="gender" required>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+        </select><br>
 
-    <label for="weight">Weight (kg):</label>
-    <input type="number" name="weight" step="0.1" required><br>
+        <label for="weight">Weight (kg):</label>
+        <input type="number" name="weight" step="0.1" required><br>
 
-    <label for="height">Height (m):</label>
-    <input type="number" name="height" step="0.01" required><br>
+        <label for="height">Height (m):</label>
+        <input type="number" name="height" step="0.01" required><br>
 
-    <input type="submit" name="submit" value="Calculate BMI">
-</form>
-<?php echo $_POST["result"]; ?>
+        <input type="submit" name="submit" value="Calculate BMI">
+    </form>
+
+    <?php echo $_POST["result"]; ?>
 
 </body>
 </html>
--- bmi/process.php
+++ bmi/process.php
@@ -1,69 +1,60 @@
 <?php
-function showBMIStatusMale($bmi) {
-    echo "<p>BMI Category: ";
-    if ($bmi < 18.5) {
-        echo "Underweight";
-    } elseif ($bmi >= 18.5 && $bmi <= 24.9) {
-        echo "Normal weight";
-    } elseif ($bmi >= 25 && $bmi <= 29.9) {
-        echo "Overweight";
-    } elseif ($bmi >= 30 && $bmi <= 34.9) {
-        echo "Obese class I";
-    } elseif ($bmi >= 35 && $bmi <= 39.9) {
-        echo "Obese class II";
-    } else {
-        echo "Obese class III";
+
+    function calculateBMI ($gender, $weight, $height) {
+        $gender = $gender;
+        $weight = floatval($weight);
+        $height = floatval($height);
+        
+        $bmi = $weight / ($height * $height);
+
+        return  number_format($bmi, 2);
     }
-    echo "</p>";
-}
 
-function showBMIStatusFemale($bmi) {
-    echo "<p>BMI Category: ";
-    if ($bmi < 18.5) {
-        echo "Underweight";
-    } elseif ($bmi >= 18.5 && $bmi <= 23.9) {
-        echo "Normal weight";
-    } elseif ($bmi >= 24 && $bmi <= 28.9) {
-        echo "Overweight";
-    } elseif ($bmi >= 29 && $bmi <= 34.9) {
-        echo "Obese class I";
-    } elseif ($bmi >= 35 && $bmi <= 39.9) {
-        echo "Obese class II";
-    } else {
-        echo "Obese class III";
+    function showBMIStatusMale($bmi) {
+        echo "<p>BMI Category: ";
+        if ($bmi < 18.5) {
+            echo "Underweight";
+        } elseif ($bmi >= 18.5 && $bmi <= 24.9) {
+            echo "Normal weight";
+        } elseif ($bmi >= 25 && $bmi <= 29.9) {
+            echo "Overweight";
+        } elseif ($bmi >= 30 && $bmi <= 34.9) {
+            echo "Obese class I";
+        } elseif ($bmi >= 35 && $bmi <= 39.9) {
+            echo "Obese class II";
+        } else {
+            echo "Obese class III";
+        }
+        echo "</p>";
     }
-    echo "</p>";
-}
 
-function calculateBMI ($gender, $weight, $height) {
-    $gender = $gender;
-    $weight = floatval($weight);
-    $height = floatval($height);
-    
-    $bmi = $weight / ($height * $height);
+    function showBMIStatusFemale($bmi) {
+        echo "<p>BMI Category: ";
+        if ($bmi < 18.5) {
+            echo "Underweight";
+        } elseif ($bmi >= 18.5 && $bmi <= 23.9) {
+            echo "Normal weight";
+        } elseif ($bmi >= 24 && $bmi <= 28.9) {
+            echo "Overweight";
+        } elseif ($bmi >= 29 && $bmi <= 34.9) {
+            echo "Obese class I";
+        } elseif ($bmi >= 35 && $bmi <= 39.9) {
+            echo "Obese class II";
+        } else {
+            echo "Obese class III";
+        }
+        echo "</p>";
+    }
 
-    return  number_format($bmi, 2);
-}
-
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-//     $gender = $_POST["gender"];
-//     $weight = floatval($_POST["weight"]);
-//     $height = floatval($_POST["height"]);
-
-//     $bmi = $weight / ($height * $height);
-
-//     $result = "Your BMI is: " . number_format($bmi, 2);
-
-//     echo "<p>$result</p>";
-
-//     if ($gender == "male") {
-//         showBMIStatusMale($bmi);
-//     } elseif ($gender == "female") {
-//         showBMIStatusFemale($bmi);
-//     }
-// }
-
-
+    function BMIStatus($gender, $bmi) {
+        if ($gender == "male") {
+            showBMIStatusMale($bmi);
+        } elseif ($gender == "female") {
+            showBMIStatusFemale($bmi);
+        }
+    }
 
 ?>
+
+
+
