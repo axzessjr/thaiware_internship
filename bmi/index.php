924508a15d160b2397fee972a8b332eaa2041218
muGGlez <p.suksawatt@gmail.com>
2024-01-15 10:50:02 +0700

    js

--- bmi/index.php
+++ bmi/index.php
@@ -1,26 +1,26 @@
 <?php
-    include "process.php";
+    // include "process.php";
 
-    if(isset($_POST["submit"])) {
-        $input_gender = $_POST["gender"];
-        $input_weight = floatval($_POST["weight"]);
-        $input_height = floatval($_POST["height"]);
+    // if(isset($_POST["submit"])) {
+    //     $input_gender = $_POST["gender"];
+    //     $input_weight = floatval($_POST["weight"]);
+    //     $input_height = floatval($_POST["height"]);
 
-        $gender = $input_gender;
-        $bmi = calculateBMI($input_gender, $input_weight, $input_height);
+    //     $gender = $input_gender;
+    //     $bmi = calculateBMI($input_gender, $input_weight, $input_height);
 
          
-        if ($gender == "male") {
-            $bmi_status = showBMIStatusMale($bmi);
-        } else
-        if ($gender == "female") {
-            $bmi_status = showBMIStatusFemale($bmi);
-        }
+    //     if ($gender == "male") {
+    //         $bmi_status = showBMIStatusMale($bmi);
+    //     } else
+    //     if ($gender == "female") {
+    //         $bmi_status = showBMIStatusFemale($bmi);
+    //     }
         
-        $result = "Your BMI: " . $bmi . " <br>Your Status: " . $bmi_status;
+    //     $result = "Your BMI: " . $bmi . " <br>Your Status: " . $bmi_status;
 
-        $_POST["result"] = $result;
-    }
+    //     $_POST["result"] = $result;
+    // }
 ?>
 
 <!DOCTYPE html>
@@ -32,28 +32,79 @@
 
     <link rel="stylesheet" href="/thaiware_internship/bmi/css/style.css" />
 
+    <!-- <script src="/thaiware_internship/bmi/js/process.js"></script>  -->
+
 </head>
 <body>
+    <script>
+        function calculateBMI(gender, weight, height) {
+            var bmi = weight / (height * height);
+            console.log (gender + " " + weight + " " + height + " " + bmi)
+            // return bmi.toFixed(2);
+           
+        }
+
+        function showBMIStatusMale(bmi) {
+            if (bmi < 18.5) {
+                return "Underweight";
+            } else if (bmi >= 18.5 && bmi <= 24.9) {
+                return "Normal weight";
+            } else if (bmi >= 25 && bmi <= 29.9) {
+                return "Overweight";
+            } else if (bmi >= 30 && bmi <= 34.9) {
+                return "Obese class I";
+            } else if (bmi >= 35 && bmi <= 39.9) {
+                return "Obese class II";
+            } else {
+                return "Obese class III";
+            }
+        }
+
+        function showBMIStatusFemale(bmi) {
+            if (bmi < 18.5) {
+                return "Underweight";
+            } else if (bmi >= 18.5 && bmi <= 23.9) {
+                return "Normal weight";
+            } else if (bmi >= 24 && bmi <= 28.9) {
+                return "Overweight";
+            } else if (bmi >= 29 && bmi <= 34.9) {
+                return "Obese class I";
+            } else if (bmi >= 35 && bmi <= 39.9) {
+                return "Obese class II";
+            } else {
+                return "Obese class III";
+            }
+        }
+
+        function BMIStatus(gender, bmi) {
+            if (gender === "male") {
+                return showBMIStatusMale(bmi);
+            } else if (gender === "female") {
+                return showBMIStatusFemale(bmi);
+            }
+        }
+
+        console
+
+    </script>
+
     <form method="post" action="">
         <label for="gender">Gender:</label>
         <select name="gender" required>
-        <option value="male"<?php if ($_POST["gender"] == "male") { echo " selected"; } ?>>Male</option>
-        <option value="female"<?php if ($_POST["gender"] == "female") { echo " selected"; } ?>>Female</option>
-           
+            <option value="male">Male</option>
+            <option value="female">Female</option>
         </select><br>
 
         <label for="weight">Weight (kg):</label>
-        <input type="number" name="weight" step="0.1" value="<?php echo $_POST["weight"]; ?>" required><br>
+        <input type="number" name="weight" step="0.1" value="" required><br>
 
         <label for="height">Height (m):</label>
-        <input type="number" name="height" step="0.01" value="<?php echo $_POST["height"]; ?>" required><br>
+        <input type="number" name="height" step="0.01" value="" required><br>
 
         <input type="submit" name="submit" value="Calculate BMI">
     </form>
     
-    <div class="rs">
-        <?php echo $_POST["result"]; ?>
-    </div>
+
    
 </body>
 </html>
