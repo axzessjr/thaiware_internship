a852965f2e742a9051c102925962eb7405dcb607
muGGlez <p.suksawatt@gmail.com>
2024-02-05 09:44:27 +0700

    day default

--- dmy/process_after.php
+++ dmy/process_after.php
@@ -6,6 +6,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $count = $_POST['count'];
     $date = date_create("$year-$month-$day");
     date_add($date, date_interval_create_from_date_string("$count days"));
-    echo "ถัดจากนี้ไปอีก $count วัน, คือวันที่ : " . date_format($date, "d/m/Y");
+    echo "ถัดจากนี้ไปอีก $count วัน, คือวันที่ : " . date_format($date, "j/n/Y");
 }
 ?>
