119d6ee61242edfcd59696208bf6ac4b0c61169e
muGGlez <p.suksawatt@gmail.com>
2024-02-08 16:18:16 +0700

    no message

--- factor/process_degree.php
+++ factor/process_degree.php
@@ -8,21 +8,21 @@ if (isset($_POST['input1']) && isset($_POST['input2']) && isset($_POST['input3']
 }
 function factor($a, $b, $c) {
     $delta = $b * $b - 4 * $a * $c;
-    if ($delta > 0) {
+    if ($delta > 0 && $a = 1) {
         $root1 = ($b + sqrt($delta)) / (2 * $a);
         $root2 = ($b - sqrt($delta)) / (2 * $a);
         $factor1 = "(x + $root1)";
         $factor2 = "(x + $root2)";
         return $factor1.$factor2;
     }
-    elseif ($delta == 0) {
+    else if ($delta == 0) {
         $root = $b / (2 * $a);
         $factor1 = "(x + $root)";
         $factor2 = "(x + $root)";
         return $factor1.$factor2;
     }
     else {
-        return "หาค่าไม่ได้";
+       return "หาค่าไม่ได้";
     }
 }
 ?>
