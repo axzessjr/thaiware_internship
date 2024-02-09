9287e121cd4871125a52fd20d0ace0f66b197d8a
JobsKrittanon <axzess@gmail.com>
2024-02-09 11:06:39 +0700

    Update process_degree.php
    corrected factor function

--- factor/process_degree.php
+++ factor/process_degree.php
@@ -10,38 +10,44 @@ if (isset($_POST['input1']) && isset($_POST['input2']) && isset($_POST['input3']
 }
 function findXY($a, $b, $c) {
     $result = array();
+    $x1y1 = array();
+    $x2y2 = array();
     for ($x1 = 1; $x1 <= $a; $x1++) {
         if ($a % $x1 == 0) {
             $y1 = $a / $x1;
-            $x1y1 = array($x1, $y1);
+            $x1y1[] = array($x1, $y1);
         }
     }
     for ($x2 = 1; $x2 <= $c; $x2++) {
         if ($c % $x2 == 0) {
             $y2 = $c / $x2;
-            $x2y2 = array($x2, $y2);
+            $x2y2[] = array($x2, $y2);
         }
     }
     foreach ($x1y1 as $point1) {
-        $x1 = $point1[0];
-        $y1 = $point1[1];
+        $x1 = intVal($point1[0]);
+        $y1 = intVal($point1[1]);
+        $resultXY = 0;
         
         foreach ($x2y2 as $point2) {
-            $x2 = $point2[0];
-            $y2 = $point2[1];
+            $x2 = intVal($point2[0]);
+            $y2 = intVal($point2[1]);
+            $B = intVal($b);
             
-            $result_x = $x1 * $x2;
-            $result_y = $y1 * $y2;
-            $result = $result_x + $result_y;
+            $product_of_A = $x1 * $y2;
+            $product_of_C = $x2 * $y1;
+            $sum_of_AC = $product_of_A + $product_of_C;
             
-            $resultB = [$result];
+            if($sum_of_AC == $B){
+                $result[] = array (
+                    "A" => array ($x1,$y1),
+                    "C" => array ($x2,$y2),
+                    "B" => $sum_of_AC
+                );
+            }
         }
+        
     }
-    // if ($resultB = $b) {
-    //     echo $resultB;
-    // }
-
-
     return $result;
 }
 // function factor($a, $b, $c) {
